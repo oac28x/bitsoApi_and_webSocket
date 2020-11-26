5f2cf432b8d9dc03721ac4a80b27cf45e68cfab1
Oscar Aguilar Cruz <oscar.aguilar@softtek.com>
2020-11-26 02:08:43 -0600

    Config Tokens data class

--- WebSocket/ConfigData.cs
+++ WebSocket/ConfigData.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 namespace WebSocket
 {
-    public class ConfigData
+    public static class ConfigData
     {
-        public ConfigData()
-        {
-        }
+        public const bool IsProduction = true;
+
+        public const string BITSO_KEY = "";
+        public const string BITSO_SECRET = "";
+
+        public const string TelegramApiToken = "";
+        public const string TelegramPublicChatId = "";
+        public const string TelegramMyChatId = "";
+
+        //Bitso available coins to pass on Init(...)
+        public static readonly string[] books =  {
+            "btc_mxn",
+            "eth_mxn",
+            "ltc_mxn",
+            "xrp_mxn",
+            "gnt_mxn",
+            "bat_mxn",
+            "bch_mxn",
+            "dai_mxn",
+            "mana_mxn"
+        };
     }
 }
